6e94e88e5dffcf9eff9f7cd08fbf2c98037d6895
godekd3133 <42081059+godekd3133@users.noreply.github.com>
2020-07-21 00:56:31 +0900

    2020/07/21 12:56 김민규 1. 파일정리 2. 빠친코 튕기는거 1차 구현 3. Dialog 딜레이방식 텍스트 연출 (데이터 연동 아직)

---
+++ Assets/Dummy(Minkyu)/Dialog/Dialog.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class MsgInfo
+{
+    public string Msg;
+    public float Delay;
+}
+public class Dialog : MonoBehaviour
+{
+    [SerializeField]
+    public List<MsgInfo> MsgQueue;
+
+     Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+    private void Start()
+    {
+       StartCoroutine(MsgQueue_Start(MsgQueue[0]));
+    }
+    private void Update()
+    {
+        
+    }
+
+    public IEnumerator MsgQueue_Start(MsgInfo Info)
+    {
+        string Temp_Msg = Info.Msg; 
+        float Delay = Info.Delay;
+
+        for(int i =0; i< Temp_Msg.Length; i++)
+        {
+            yield return new WaitForSeconds(Delay);
+            text.text += Temp_Msg[i];
+        }
+        yield return null;
+    }
+    
+}
---
+++ Assets/Dummy(Minkyu)/NumberScroll/PriceScroll.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriceScroll : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform ScrollObject01;
+
+    [SerializeField]
+    RectTransform ScrollObject02;
+
+    [SerializeField]
+    RectTransform Bottom;
+
+    [SerializeField]
+    RectTransform Top;
+
+    [SerializeField]
+    CenterLine CenterLine;
+
+    Vector3 OldPos = Vector3.zero;
+    Vector3 Pos = Vector3.zero;
+
+
+   public  float Power;
+    bool Scroll = false;
+    float fTime = 0f;
+    float ScrollTime = 0f;
+    bool Bounce = false;
+    float Dis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Power = -5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Power *= 0.99f;
+        OldPos = Pos;
+        Pos = Input.mousePosition;
+        if (Input.GetMouseButton(0))
+        {
+
+            float delta =  OldPos.y - Pos.y;
+            Scroll_By(delta);
+            Scroll = true;
+        }
+        Power *= 0.99f;
+
+        if (Scroll == true)
+        {
+            ScrollTime += Time.deltaTime;
+
+            if(ScrollTime > 0.5f && Mathf.Abs(Power) < 0.4f)
+            {
+                if (Bounce == false)
+                {
+                    Bounce = true;
+                    Dis = CenterLine.transform.position.y - CenterLine.CollisionObject.position.y;
+                }
+              
+            }
+
+            if(Bounce == false)
+            Scroll_Move(Power * Time.deltaTime);
+            else
+            {
+                //n의 길이를 s초동안 이동
+                float temp = Dis;
+                fTime += Time.deltaTime;
+                Dis = Mathf.SmoothStep(temp, 0, fTime/ 2f);
+
+                Scroll_Move(Dis - temp);
+
+                if (Mathf.Abs(Dis) < 0.1f)
+                {
+                    Bounce = false;
+                    Scroll = false;
+                    Power = 0f;
+                    fTime = 0f;
+                    ScrollTime = 0f;
+                }
+            }
+            
+        }
+
+    }
+    public void Scroll_Move(float _Distance)
+    {
+        ScrollObject01.transform.position += Vector3.down * _Distance;
+        ScrollObject02.transform.position += Vector3.down * _Distance;
+
+        if (ScrollObject01.anchoredPosition.y < Bottom.anchoredPosition.y)
+        {
+            ScrollObject01.anchoredPosition += new Vector2(0, 2000);
+        }
+        if (ScrollObject02.anchoredPosition.y < Bottom.anchoredPosition.y)
+        {
+            ScrollObject02.anchoredPosition += new Vector2(0, 2000);
+        }
+
+        if (ScrollObject01.anchoredPosition.y > Top.anchoredPosition.y)
+        {
+            ScrollObject01.anchoredPosition -= new Vector2(0, 2000);
+        }
+        if (ScrollObject02.anchoredPosition.y > Top.anchoredPosition.y)
+        {
+            ScrollObject02.anchoredPosition -= new Vector2(0, 2000);
+        }
+    }
+
+    public void Scroll_By(float _Power)
+    {
+        Power = _Power;
+    }
+
+    public void Scroll_To(int _Num)
+    {
+
+    }
+}
